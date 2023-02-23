ad46b4be73c56b56ec873654d407dbba2a5c9a82
priyasingh890 <110822414+priyasingh890@users.noreply.github.com>
2023-02-23 09:30:34 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,65 @@
+# i=0
+# while i<10:
+#     i=i+1
+#     if i==3:
+#         break
+#     print(i)
+
+# i=0
+# while i<5:
+#     if i==3:
+#         break
+#     print(i)
+#     i=i+1
+
+# i=0
+# while i<6:
+#     if i==5:
+#         print(i)
+#         break
+#     i=i+1
+# 
+# k=[]
+# j=int(input("enter no"))
+# i=0
+# while i<j:
+#     h=int(input("enter no."))
+#     if h==1:
+#         print("one")
+#     elif h==2:
+#         print("two")
+#     else:
+#         print("three")
+#     i=i+1
+
+# d=int(input("enter no"))
+# s=(str(d))
+# i=0
+# while i<len(s):
+#     if s[i]=="1":
+#         print("one",end=" ")
+#     if s[i]=="2":
+#         print("two",end=" ")
+#     if s[i]=="3":
+#         print("three",end=" ")
+#     if s[i]=="4":
+#         print("four",end=" ")
+#     i=i+1
+    
+# a=(input("enter no"))
+# i=0
+# j=1
+# sum=0
+# while i<len(a):
+#     sum=sum+(1*2**i)*int(a[-j])
+#     j=j+1
+#     i=i+1
+# print(sum)
+
+    
+    
+    
+
+    
+
+
